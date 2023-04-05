191a94082ab93b88a732f461135664b18de8d917
Olivier Vignaud <ovi@ubiqube.com>
2023-04-05 16:45:26 +0200

    helm3-webapp: Change Oauth2 configurer.

--- helm3-webapp/src/main/java/com/ubiqube/helm/AuthConfig.java
+++ helm3-webapp/src/main/java/com/ubiqube/helm/AuthConfig.java
@@ -1,8 +1,25 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.ubiqube.helm;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -16,8 +33,7 @@ public class AuthConfig {
 				.requestMatchers("/**").hasAnyAuthority("SCOPE_helm")
 				.anyRequest().authenticated()
 				.and()
-				.oauth2ResourceServer()
-				.jwt();
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		return http.build();
 	}
 }
